92e45c564e3d86ee401f048dbd92d9c8b0b39ead
vvveiii <cntrump@gmail.com>
2019-05-21 14:10:13 +0800

    swift 5;
    static link;
    remove sb for example.

--- iOS Example/iOS Example/ViewController.swift
+++ iOS Example/iOS Example/ViewController.swift
@@ -14,17 +14,45 @@ class ViewController: UIViewController {
 
 	let lexer = SwiftLexer()
 	
-	@IBOutlet weak var syntaxTextView: SyntaxTextView!
+    lazy var syntaxTextView = SyntaxTextView()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isTranslucent = true
+        navigationItem.title = "Source Editor"
+
+        view.addSubview(syntaxTextView)
+        syntaxTextView.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraints([
+            NSLayoutConstraint(item: syntaxTextView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: syntaxTextView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: syntaxTextView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: syntaxTextView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1, constant: 0)
+            ])
+        
 		syntaxTextView.theme = DefaultSourceCodeTheme()
 		syntaxTextView.delegate = self
-
+        syntaxTextView.text = "/*\n" +
+        "Hello world\n" +
+        "*/\n" +
+        "\n" +
+        "let name = <#my name#>\n" +
+        "\n" +
+        "// Multi-line strings are awesome!\n" +
+        "\"\"\"\n" +
+        "{\n" +
+        "\"isAwesome\": true\n" +
+        "}\n" +
+        "\"\"\"\n" +
+        "\n" +
+        "guard !name.isEmpty else {\n" +
+        "    return\n" +
+        "}\n" +
+        "\n" +
+        "print(\"My name is \" + name)"
 	}
-
-
 }
 
 extension ViewController: SyntaxTextViewDelegate {
